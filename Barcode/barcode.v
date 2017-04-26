8d80bca7fac81a294e4d76255f95fc7d1c933f7f
sadeq-hashemi <hasheminejad@wisc.edu>
2017-04-26 16:45:40 -0500

    Add files via upload

---
+++ Barcode/barcode.v
@@ -0,0 +1,14 @@
+module barcode(clk, rst_n, BC, clr_ID_vld, ID, ID_vld); 
+
+input clk, rst_n
+input BC, clr_ID_vld; 
+output [7:0] ID; 
+output ID_vld; 
+
+reg  
+reg 
+localparam byte_done 4'b1000
+
+
+
+endmodule
